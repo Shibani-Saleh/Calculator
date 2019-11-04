ac2f83e498edd06324d5efde3f574b8dc701972c
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-11-04 23:29:46 +0200

    9

--- Calculator.js
+++ Calculator.js
@@ -1,72 +1,14 @@
-// var input = "";
-// var firstNum = "";
-// var secondNum = "";
-// var answer = "";
-
-// $('.num').click(function(){
-// 	$('#result').text(input += $(this).text())
-// })
-
-// function oper(){
-// 	var firstNum = input;
-// 	$('#result').empty()
-// 	$('.num').click(function(){
-// 		$('result').text(secondNum += $(this).text())
-// 	});
-// }
-
-// $("#mult").click(function(){
-//   if(input !== ""){
-//     oper();
-//   }
-// });
-
-// $("#equal").click(function(){
-//   answer = firstNum * secondNum;
-//   $("#result").text(answer);
-// });
-
-// $("#clear").click(function(){
-//   input = "";
-//   firstInput = "";
-//   secondInput = "";
-//   answer ="";
-//   $("#result").empty(); 
-// });
-
-
-// // $('#plus').click(function(){
-// // 	$('#display').append('+')
-// // })
-// // $('#equal').click(function(){
-// // 	$('#display').append('=')
-// // })
-// // $('#minus').click(function(){
-// // 	$('#display').append('-')
-// // })
-// // $('#divide').click(function(){
-// // 	$('#display').append('÷')
-// // })
-// // $('#mult').click(function(){
-// // 	$('#display').append('*')
-// // })
-
-// $("#delete").on("click",function(){
-// 	$('#display').empty(); 
-// });
-
-
 $(document).ready(function(){
 	function calculator(){
     var sum = "";
     var len;
-    var arr= [];
-    var operators = ["+", "-", "*", "/"];
+    //var arr= [];
+    var operators = ["+", "-", "*", "/", "%"];
     var input = $('#result')
     $(".num").click(function() {
     	var number = $(this).attr('value');
-    	sum = sum + number;
-        arr.push(number);
+    	sum += number;
+       // arr.push(number);
     	$('#result').html(sum)
         len = input.innerHTML.split("");
         //console.log(len);
@@ -74,16 +16,16 @@ $(document).ready(function(){
     });
     $(".operators").click(function(e) {
       e.preventDefault();
-      var ops = $(this).attr('value');
-      sum += ops;
-      arr.push(number);
+      var operator = $(this).attr('value');
+      sum += operator;
+      //arr.push(number);
         $("#result").html(sum);
        len = input.innerHTML;
-      if(/(?=(\D{2}))/g.test(sum)) {
+     // if(/(?=(\D{2}))/g.test(sum)) {
         sum = len.substring(0, len.length - 1);
         $("#result").html(sum);
-      }
-      len = input.innerHTML.split("");
+     // }
+     // len = input.innerHTML.split("");
         //console.log(len);
         
       //console.log(arr);
@@ -92,7 +34,7 @@ $(document).ready(function(){
 
         $("#equal").click(function() {
       var total =  eval(sum);
-      $("#result").attr('value', total);
+     // $("#result").attr('value', total);
       $("#result").html(total % 1 != 0 ? total.toFixed(2) : total);
     });
 
