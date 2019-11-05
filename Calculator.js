738d349b28a4453b1cf02e0a9febb94fa8b4d916
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-05 12:50:48 +0100

    10

--- Calculator.js
+++ Calculator.js
@@ -1,47 +1,44 @@
 $(document).ready(function(){
 	function calculator(){
     var sum = "";
-    var len;
-    //var arr= [];
-    var operators = ["+", "-", "*", "/", "%"];
+    //var divider;
+    //var operators = ["+", "-", "*", "/", "%", "."];
     var input = $('#result')
+     $("#result").html('Enter a number');
+
     $(".num").click(function() {
     	var number = $(this).attr('value');
     	sum += number;
-       // arr.push(number);
+       
     	$('#result').html(sum)
-        len = input.innerHTML.split("");
-        //console.log(len);
-        //console.log(arr);
+       // divider = input.innerHTML.split("");
+        
     });
-    $(".operators").click(function(e) {
-      e.preventDefault();
+
+       
+    $(".operators").click(function(event) {
+      event.preventDefault();
       var operator = $(this).attr('value');
       sum += operator;
-      //arr.push(number);
+      
+      $("#result").html(sum);
+       // divider = input.innerHTML;
+     //if(/(?=(\D{2}))/g.test(sum)) {
+        //sum = len.substring(0, divider.length - 1);
         $("#result").html(sum);
-       len = input.innerHTML;
-     // if(/(?=(\D{2}))/g.test(sum)) {
-        sum = len.substring(0, len.length - 1);
-        $("#result").html(sum);
-     // }
-     // len = input.innerHTML.split("");
-        //console.log(len);
-        
-      //console.log(arr);
+        //}
 
     });
 
         $("#equal").click(function() {
       var total =  eval(sum);
-     // $("#result").attr('value', total);
       $("#result").html(total % 1 != 0 ? total.toFixed(2) : total);
+
     });
 
     $("#clear").click(function() {
        sum = "";
-       arr = [];
-        $("#result").html(0);
+        $("#result").html('Enter a number');
     });
 
     };
